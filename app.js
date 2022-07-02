5428bbe79a032364bee4cbd484f3c0f2d1607f29
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2022-07-02 14:54:08 -0300

    adicinando funcionalidades

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let banco = [
-    { 'tarefa': 'Estudar Express', 'status': 'checked' },
+    { 'tarefa': 'Estudar Express', 'status': '' },
     {'tarefa': 'Ver Strager Things', 'status': ''}
 ]
 
-const criarItem = (tarefa, status) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
 
     item.classList.add('todo-item');
     item.innerHTML = `
-                            <input type="checkbox" ${status}>
+                            <input type="checkbox" ${status} data-indice = ${indice}>
                             <div>${tarefa}</div>
-                            <input type="button" value="X">
+                            <input type="button" value="X" data-indice = ${indice}>
                     `
     document.getElementById('todolist').appendChild(item);
 }
@@ -24,7 +24,7 @@ const limparTarefa = () => {
 
 const atualizarTela = () => {
     limparTarefa()
-    banco.forEach (item => criarItem (item.tarefa, item.status));
+    banco.forEach ((item, indice) => criarItem (item.tarefa, item.status, indice));
 }
 
 const inserirItem = (evento) => {
@@ -35,11 +35,41 @@ const inserirItem = (evento) => {
         banco.push( {'tarefa': texto, 'status': ''});
         atualizarTela();
 
-        evento.target.value = '';
+        evento.target.value = ''; //limpar tarefa
+    }
+}
+
+const removerItem = (indice) => {  //programa a função deletar da lista
+    banco.splice (indice, 1);
+    atualizarTela()
+}
+
+const atualizarItem = (indice) => {
+
+    if (banco[indice].status === '') {
+        banco[indice].status = 'checked'
+    } else {
+        banco[indice].status = ''
+    }
+
+    /* banco[indice].status = banco[indice].status == '' ? 'cheked' : ''; */
+    atualizarTela()
+}
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem(indice);
+    } else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem (indice);
     }
 }
 
 document.getElementById('new-item').addEventListener('keypress', inserirItem);
+document.getElementById('todolist').addEventListener('click', clickItem);
+
 
 atualizarTela();
 
